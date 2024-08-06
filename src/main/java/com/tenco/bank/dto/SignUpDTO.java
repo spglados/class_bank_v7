356d074eeb0aab7e755b1787e89a5549c206fe7e
lee <spglados@gmail.com>
2024-08-06 17:02:45 +0900

    h2 처리

--- src/main/java/com/tenco/bank/dto/SignUpDTO.java
+++ src/main/java/com/tenco/bank/dto/SignUpDTO.java
@@ -1,5 +1,7 @@
 package com.tenco.bank.dto;
 
+import com.tenco.bank.repository.model.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +16,15 @@ public class SignUpDTO {
 	private String username;
 	private String password;
 	private String fullname;
+	
+	// 2단계 로직 - User Object 반환
+	public User toUser() {
+		return User.builder()
+				.username(this.username)
+				.password(this.password)
+				.fullname(this.fullname)
+				.build();
+	}
 
 	// todo - 추후 사진 업로드 기능 추가 예정
 }
