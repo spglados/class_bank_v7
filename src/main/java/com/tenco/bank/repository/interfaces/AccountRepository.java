9be4f2341850ff30d53ffbf458d97f23dabb3582
lee <spglados@gmail.com>
2024-08-12 11:05:50 +0900

    통장디테일기능구현

--- src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.mysql.cj.result.IntegerValueFactory;
 import com.tenco.bank.repository.model.Account;
 
 //AccountRepository 인터페이스와 account.xml 파일을 매칭 시킨다. 
@@ -21,4 +22,6 @@ public interface AccountRepository {
 	public Account findByNumber(@Param("number") String id);
 	
 	// 코드 추가 예정
+	public Account findByAccountId(Integer accountId);
+	
 }
--- src/main/java/com/tenco/bank/service/AccountService.java
+++ src/main/java/com/tenco/bank/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.tenco.bank.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
@@ -17,6 +18,7 @@ import com.tenco.bank.repository.interfaces.AccountRepository;
 import com.tenco.bank.repository.interfaces.HistoryRepository;
 import com.tenco.bank.repository.model.Account;
 import com.tenco.bank.repository.model.History;
+import com.tenco.bank.repository.model.HistoryAccount;
 import com.tenco.bank.utils.Define;
 
 @Service
@@ -204,5 +206,30 @@ public class AccountService {
          }
     	
     }
-
+    
+    /**
+     * 단일 계좌 조회 기능
+     * @param accountId (pk) 
+     * @return
+     */
+    public Account readAccountById(Integer accountId) {
+    	Account accountEntity = accountRepository.findByAccountId(accountId);
+    	if(accountEntity == null) {
+    		throw new DataDeliveryException(Define.NOT_EXIST_ACCOUNT, HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
+    	return accountEntity;
+    }
+    
+    /**
+     *  단일 계좌 거래 내역 조회
+     * @param type = [all, deposit, withdraw]
+     * @param accountId (pk)
+     * @return 전체, 입금, 출금 거래내역(3가지 타입) 반환
+     */
+    // @Transactional
+    public List<HistoryAccount> readHistoryByAccountId(String type, Integer accountId) {
+    	List<HistoryAccount> list = new ArrayList<>();
+    	list = historyRepository.findByAccountIdAndTypeOfHistory(type, accountId); 
+    	return list;
+    }
 }
